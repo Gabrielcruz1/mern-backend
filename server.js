f8e1260d7f5bbbc38b31a7035a4c91ddb8b5cf73
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-04-11 20:26:03 -0700

    initialized socket.io instance, listened on connection

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const PORT = 12000
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 
 // app.get('/', (req, res) => {
 //     res.send('<h1>Hello World</h1>')
@@ -11,5 +14,9 @@ const server = http.createServer(app);
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 });
+
+io.on('connection', (socket) =>{
+    console.log(' a user connected');
+})
 
 app.listen(PORT, () => console.log(`Listening for client requests on port ${PORT} `));
