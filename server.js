f4392e8f25aa035375f5d19bdc6dbd55d5a7baa0
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-04-11 20:17:59 -0700

    added index html file/ changed route to send file

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const PORT = 12000
 const http = require('http');
 const server = http.createServer(app);
 
+// app.get('/', (req, res) => {
+//     res.send('<h1>Hello World</h1>')
+// });
+
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
+    res.sendFile(__dirname + '/index.html')
 });
 
 app.listen(PORT, () => console.log(`Listening for client requests on port ${PORT} `));
