720f5125c57c27256d88899911f37ec412edff69
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-04-11 20:13:48 -0700

    hello world route

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ const app = express();
 
 const PORT = 12000
 
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>')
+});
+
 app.listen(PORT, () => console.log(`Listening for client requests on port ${PORT} `));
