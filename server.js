5bfd29e14ec6d6f90e451a064e7e1c6080d7ab2c
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-04-16 15:56:53 -0700

    added dependencies, removed indexhtml file

--- server.js
+++ server.js
@@ -4,12 +4,8 @@ const PORT = 12000
 
 require('./config/db.connection');
 
-// app.get('/', (req, res) => {
-//     res.send('<h1>Hello World</h1>')
-// });
-
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    res.send('<h1>Hello World</h1>')
 });
 
 app.listen(PORT, () => console.log(`Listening for client requests on port ${PORT} `));
