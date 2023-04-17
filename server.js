9ce0969107b41fd93d0e980a2317359ad563f014
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-04-16 22:31:54 -0700

    404 route input

--- server.js
+++ server.js
@@ -8,4 +8,9 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>')
 });
 
+// 404
+app.all("/*", (req, res) => {
+    return res.status(404).json({error:"No resource found"});
+  });
+
 app.listen(PORT, () => console.log(`Listening for client requests on port ${PORT} `));
