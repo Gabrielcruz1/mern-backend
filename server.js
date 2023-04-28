2163752b39e1ecc086a871f3c802e11e197a93e1
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-04-28 07:50:08 -0700

    new user created in postman

--- server.js
+++ server.js
@@ -13,12 +13,14 @@ require("./config/db.connection")
 const { PORT, MONGODB_URI } = process.env;
 
 //IMPORT CONTROLLER
+const authController = require('./controllers/auth')
 const postController = require('./controllers/post_controller')
 
 //MIDDLEWARE
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json());
+app.use('/auth', authController)
 app.use('/posts', postController)
 
 app.get("/", (req, res) => {
@@ -27,7 +29,7 @@ app.get("/", (req, res) => {
 
 // 404
 app.all("/*", (req, res) => {
-    return res.status(404).json({error:"No resource found"});
-  });
+    return res.status(404).json({ error: "No resource found" });
+});
 
 app.listen(PORT, () => console.log(`Listening for client requests on port ${PORT} `));
