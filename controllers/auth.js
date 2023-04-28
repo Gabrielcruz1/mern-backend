942a835a621fe7da1662659cff2c4845b8aa9398
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-04-28 07:28:14 -0700

    auth.js controller added no routes

---
+++ controllers/auth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+
+//controllers/ routes
+
+
+module.exports = router
