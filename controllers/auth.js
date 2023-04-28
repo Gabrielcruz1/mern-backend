b6b55ad92da2e38100c1d1f7d54805d157d8152a
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-04-28 07:34:58 -0700

    post route try catch input

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,32 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 
-//controllers/ routes
+// SIGN UP
+// POST /auth/register
+router.post("/register", async (req, res, next) => {
+    //   has the password before storing the user info in the database
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(req.body.password, salt);
+
+        req.body.password = passwordHash;
+
+        const newUser = await User.create(req.body);
+
+        res.status(201).json({
+            user: newUser,
+            isLoggedIn: true,
+        });
+    } catch (err) {
+        res.status(400).json({ err: err.message });
+    }
+});
+
+// SIGN IN
+// POST /auth/login
+router.post("/login", async (req, res, next) => { });
+
+
 
 
 module.exports = router
