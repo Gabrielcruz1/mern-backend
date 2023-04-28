f03ec1c24262eec48b91d3d9a46db45b9af6222a
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-04-28 09:58:59 -0700

    added .populate to index and show routes. working in postman

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -5,7 +5,7 @@ const { Post } = require('../models/index')
 //INDEX
 router.get('/', async (req, res, next) => {
     try {
-        const allPosts = await Post.find({});
+        const allPosts = await Post.find({}).populate('owner', 'username -_id').exec();
         return res.status(200).json(allPosts)
     } catch (err) {
         res.status(400).json({ error: "error" })
@@ -26,11 +26,13 @@ router.post('/', async (req, res, next) => {
 
 //SHOW
 router.get('/:id', async (req, res, next) => {
-    try{
+    try {
         const foundPost = await Post.findById(req.params.id)
+            .populate("owner")
+            .exec();
         res.status(200).json(foundPost)
-    } catch(err){
-		res.status(400).json({error: "error"})
+    } catch (err) {
+        res.status(400).json({ error: "error" })
         return next(err)
     }
 });
@@ -38,10 +40,10 @@ router.get('/:id', async (req, res, next) => {
 //UPDATE
 router.put('/:id', async (req, res, next) => {
     try {
-        const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true });
         return res.status(200).json(updatedPost)
-    } catch(err){
-		res.status(400).json({error: "error"})
+    } catch (err) {
+        res.status(400).json({ error: "error" })
         return next(err)
     }
 });
@@ -51,8 +53,8 @@ router.delete('/:id', async (req, res, next) => {
     try {
         const deletedPost = await Post.findByIdAndDelete(req.params.id);
         res.redirect('/posts')
-    } catch(err){
-		res.status(400).json({error: "error"})
+    } catch (err) {
+        res.status(400).json({ error: "error" })
         return next(err)
     }
 });
